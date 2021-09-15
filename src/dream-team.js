dbb6bd70b420a0bae737d5e6fcca8935910841d1
baur <Bauyrzhan.Yessetov@gmail.com>
2021-09-15 18:14:59 +0600

    createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  let result = []
+  members.forEach(member => {
+    if ((typeof member) === 'string') {
+      result.push(member.replaceAll(' ', '')[0].toUpperCase())
+    }
+  });
+  return result.sort().join('')
 }
